a5d3f55e98fdf30997529ed4ac40ae85b9e97cb4
PabloMarcano <pablo.marcano.16@gmail.com>
2020-06-05 15:08:20 -0400

    install ecommerce

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://covid19.islapixel.com/admin/');
+define('HTTP_CATALOG', 'https://covid19.islapixel.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://covid19.islapixel.com/admin/');
+define('HTTPS_CATALOG', 'https://covid19.islapixel.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/islapixe/covid19/admin/');
+define('DIR_SYSTEM', '/home/islapixe/covid19/system/');
+define('DIR_IMAGE', '/home/islapixe/covid19/image/');
+define('DIR_STORAGE', '/home/islapixe/ocartdata/storage/');
+define('DIR_CATALOG', '/home/islapixe/covid19/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'islapixe_covid19');
+define('DB_PASSWORD', '05TS(p@ac6');
+define('DB_DATABASE', 'islapixe_covid19');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'covid19_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
